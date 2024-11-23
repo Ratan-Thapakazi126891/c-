099ae3d00b1ae0c792078782921d38c8d75f505a
Ratan Thapa <122299475+Ratan-Thapakazi126891@users.noreply.github.com>
2024-11-23 12:24:22 +0545

    Add files via upload

---
+++ bisection.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define e 0.00003
+ void main()
+ {
+ 	float a,b,c,fa,fb,fc,e;
+ 	int i=1;
+ 	printf("enter the intial guess of a and b :");
+ 	scanf("%d%d",&a,&b);
+ 	fa=f(a);
+ 	fb=f(b);
+ 
+ 	if(fa * fb > 0)
+ 	{
+ 		printf("invalid guess"):
+ 			
+	 }
+	 else
+ 	{
+ 		printf(" %f\t%f\t%f\t%f\t%f\t%f\t%f\t,a,b,c,fa,fb,fc,e  ");
+ 		
+ 		
+ 		do
+		 {
+ 		   if((a)*f(c)<0)
+ 	 {
+	  b=c;
+}
+ 	 else 
+	  {
+	  
+ 	 a=c;
+ }
+ 		  
+ 			  
+ 			  
+ 			  
+		 }
+ 		
+	 }
+ 	
+    if((a)*f(c)<0)
+ 	 {
+	  b=c;
+}
+ 	 else 
+	  {
+	  
+ 	 a=c;
+ }
+ 	
+  } 
+
